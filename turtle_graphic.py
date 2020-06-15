dd24e12b9cf9ddc5230f0b327c66515f278039a8
mukul singh <51323825+mukulsingh94868@users.noreply.github.com>
2020-06-15 15:45:48 +0530

    Add files via upload

---
+++ turtle_graphic.py
@@ -0,0 +1,18 @@
+import turtle as t
+for i in range(1,40):
+    t.color('black')
+    t.forward(200)
+    t.right(90)
+    t.speed(100)
+    t.color('red')
+    t.circle(i)
+
+
+
+t.goto(-60,-60)
+
+for j in range(1,20):
+    t.forward(150)
+    t.speed(10)
+    t.left(135)
+
